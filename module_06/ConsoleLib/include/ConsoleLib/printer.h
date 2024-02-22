c371acf63aaf875fcde3b829edad3e763a8457e7
christian98gm <christian98gm@gmail.com>
2024-02-22 18:12:44 +0100

    Module 06

---
+++ module_06/ConsoleLib/include/ConsoleLib/printer.h
@@ -0,0 +1,11 @@
+#ifndef _CONSOLE_LIB_PRINTER_H_
+#define _CONSOLE_LIB_PRINTER_H_
+
+#include <string>
+
+void print(std::string);
+void printResult(std::string, float);
+
+void printEndl();
+
+#endif
